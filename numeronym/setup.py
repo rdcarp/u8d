7b2e5960165e24289cfcb6eea839a86ae8861c02
Richard Carp <richard.carp@clinithink.com>
2018-11-24 01:39:00 +0000

    add fire to setup install requirements

--- numeronym/setup.py
+++ numeronym/setup.py
@@ -27,7 +27,7 @@ setup(
 
     packages=find_packages(exclude=('tests',)),
 
-    install_requires=[],
+    install_requires=["fire"],
 
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
